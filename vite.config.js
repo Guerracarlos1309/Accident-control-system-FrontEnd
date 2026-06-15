ec97f35123b9972d42d5275d08c5fa2e920a5195
Guerracarlos1309 <guerracarlos1309@gmail.com>
2026-06-15 10:49:31 -0400

    chore: remove Tauri build configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "../Sistema/Accidentes e inspecciones ASHO/src-tauri/dist",
-    emptyOutDir: true,
-  },
 });
